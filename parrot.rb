7ca1f6fddd162f555266d980ae188f1af7b09c73
deezy13 <deezy13@gmail.com>
2019-10-10 16:40:40 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,14 +2,8 @@
 # returns the phrase
 
 
-def parrot
-  phrase = "Squawk!"
+def parrot(phrase = "Squawk!")
+  
   puts phrase
   return phrase
-end 
-
-def parrot(phrase)
-  new_phrase = phrase
-  puts new_phrase
-  return new_phrase
 end 
