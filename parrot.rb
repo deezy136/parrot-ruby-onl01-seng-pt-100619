924beac40b5767eb2f551778c38d055a3b0bdb7c
deezy13 <deezy13@gmail.com>
2019-10-10 16:39:16 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
+
+def parrot
+  phrase
+  puts new_phrase
+  return new_phrase
+end 
 def parrot(phrase)
   new_phrase = phrase
   puts new_phrase
